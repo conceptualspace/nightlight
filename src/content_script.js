26a95a9a56f9d0d924992e8914ec607803d16783
conceptualspace <dev@conceptualspace.net>
2017-09-14 17:06:47 -0400

    set style inline
    > test setting the filter via root element.style property instead of
    inserting a css node
    > simplifies code
    > any difference in performance?
    > todo: exclude images again

--- src/content_script.js
+++ src/content_script.js
@@ -6,28 +6,18 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     invert(request.response);
 });
 
-var css = null;
-var initialBackgroundColor = document.body.style.backgroundColor || "initial";
+var bgBlack = document.documentElement.style.backgroundColor || 'black';
+var bgWhite = document.documentElement.style.backgroundColor || 'white';
 
 function invert(status) {
     if (status === "enabled") {
-        css = 'html {filter: invert(90%) hue-rotate(180deg);} img {filter: invert(90%) hue-rotate(180deg); }';
-        document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
+        document.documentElement.style.backgroundColor = bgBlack;
+        document.documentElement.style.filter = "hue-rotate(180deg) invert(100%)";
+        //document.body.style.backgroundColor = "white";
+        //document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
     } else {
-        var css ='html {filter: invert(0%) hue-rotate(0deg); } img {filter: invert(0%); }';
-        document.body.style.backgroundColor = initialBackgroundColor;
+        document.documentElement.style.backgroundColor = bgWhite;
+        document.documentElement.style.filter = "hue-rotate(0deg) invert(0%)";
+        //document.body.style.backgroundColor = initialBackgroundColor;
     }
-
-    var head = document.getElementsByTagName('head')[0];
-    var style = document.createElement('style');
-
-    style.type = 'text/css';
-    if (style.styleSheet){
-        style.styleSheet.cssText = css;
-    } else {
-        style.appendChild(document.createTextNode(css));
-    }
-
-    //injecting the css to the head
-    head.appendChild(style);
 }
